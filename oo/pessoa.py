feb0d1e6aa9be64dc40400f202df56281810b474
Marcelo-Conti <mar.conti@yahoo.com.br>
2020-04-03 16:31:30 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=55):
+    def __init__(self, *filhos, nome=None, idade=55):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá{id(self)}'
 
 if __name__=='__main__':
-    p = Pessoa('Marcelo')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Marcelo'
-    print(p.nome)
-    print(p.idade)
+    Ana = Pessoa(nome='Ana')
+    Bruna = Pessoa(nome='Bruna')
+    Marcelo = Pessoa(Ana,Bruna,nome='Marcelo')
+    print(Pessoa.cumprimentar(Marcelo))
+    print(id(Marcelo))
+    print(Marcelo.cumprimentar())
+    print(Marcelo.nome)
+    print(Marcelo.idade)
+    for filhos in Marcelo.filhos:
+        print(filhos.nome)
