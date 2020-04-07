794fdb8eb8814bc52bb04bb9789f4f587c18ec1f
Marcelo-Conti <mar.conti@yahoo.com.br>
2020-04-07 16:09:42 -0300

    Implementação de carro, motor e direção

--- oo/carro.py
+++ oo/carro.py
@@ -19,31 +19,33 @@ A Direção oferece os seguintes atributos:
    Exemplo:
    #Testando motor
    >>> motor = Motor()
-   >>> motor.velocidade()
+   >>> motor.velocidade
    0
    >>> motor.acelerar()
-   >>> motor.velocidade()
+   >>> motor.velocidade
    1
    >>> motor.acelerar()
-   >>> motor.velocidade()
+   >>> motor.velocidade
    2
    >>> motor.acelerar()
-   >>> motor.velocidade()
+   >>> motor.velocidade
    3
    >>> motor.frear()
-   >>> motor.velocidade()
+   >>> motor.velocidade
    1
    >>> motor.frear()
-   >>> motor.velocidade()
+   >>> motor.velocidade
    0
+
    #Testando Direção
    >>> direcao = Direcao()
-   >>> direcao.Valor
+   >>> direcao.valor
    'Norte'
    >>> direcao.girar_a_direita()
-   >>> direcao.Valor
+   >>> direcao.valor
    'Leste'
    >>> direcao.girar_a_esquerda()
+   >>> direcao.valor
    'Norte'
    >>> carro = Carro(direcao, motor)
    >>> carro.calcular_velocidade()
@@ -69,3 +71,57 @@ A Direção oferece os seguintes atributos:
    >>> carro.calcular_direcao()
    'Oeste'
 """
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
+NORTE = 'Norte'
+SUL = 'Sul'
+OESTE = 'Oeste'
+LESTE = 'Leste'
+
+class Direcao:
+    rotacao_a_direita_dct = {NORTE: LESTE,LESTE: SUL,SUL: OESTE,OESTE: NORTE}
+    rotacao_a_esquerda_dct = {NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE}
+    def __init__(self):
+        self.valor = NORTE
+
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
+
+
+
+
+class Motor:
+    def __init__(self):
+        self.velocidade = 0
+
+    def acelerar(self):
+        self.velocidade+=1
+        return
+    def frear(self):
+        self.velocidade-=2
+        self.velocidade = max(0,self.velocidade)
