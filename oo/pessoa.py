a93b7ab1044b4eece430b088fb4993c2be8269ad
Marcelo-Conti <mar.conti@yahoo.com.br>
2020-04-07 11:27:29 -0300

    Criado atributo de classe olhos @classmethod

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -8,6 +8,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá{id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 if __name__=='__main__':
     Ana = Pessoa(nome='Ana')
     Bruna = Pessoa(nome='Bruna')
@@ -29,3 +37,5 @@ if __name__=='__main__':
     print(Pessoa.olhos)
     print(Ana.olhos)
     print(id(Pessoa.olhos),id(Ana.olhos))
+    print(Pessoa.metodo_estatico(), Marcelo.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), Marcelo.nome_e_atributos_de_classe())
